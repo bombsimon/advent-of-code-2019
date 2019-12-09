7405b3f0f00ac34b20a19e2913fd17a4c486acb5
Simon Sawert <simon@sawert.se>
2019-12-09 17:51:06 +0100

    Refactor day 9, DRY etc

--- 09/go/main.go
+++ 09/go/main.go
@@ -3,21 +3,16 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-// paramMode represents the parameter mode which can be positional (the value at
-// the position) or immediate (the actual value).
-type paramMode int
-
-// * ParamModePosition -> Use the value found at slice[n]
+// * ParamModePosition  -> Use the value found at slice[n]
 // * ParamModeImmediate -> Use the value n
-// * ParamModeRelative -> Use the value found at slice[BASE+n]
+// * ParamModeRelative  -> Use the value found at slice[BASE+n]
 const (
-	paramModePosition paramMode = iota
+	paramModePosition = iota
 	paramModeImmediate
 	paramModeRelative
 )
@@ -35,65 +30,59 @@ const (
 	opCodeHalt        = 99
 )
 
+// nolint: gochecknoglobals
+var jumpMap = map[int]int{
+	opCodeAdd:         4,
+	opCodeMultiply:    4,
+	opCodeStore:       2,
+	opCodeOutput:      2,
+	opCodeJumpIfTrue:  3,
+	opCodeJumpIfFalse: 3,
+	opCodeLessThan:    4,
+	opCodeEquals:      4,
+	opCodeAdjustBase:  2,
+}
+
 type computer struct {
-	Name       string
-	Phase      int
-	Input      int
-	InputCount int
-	Output     []int
-	Pointer    int
-	Base       int
-	Sequence   []int
-	Halted     bool
+	Input    int
+	Output   []int
+	Pointer  int
+	Base     int
+	Sequence []int
+	Halted   bool
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("missing file as input")
-	}
-
-	line, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatalf("could not read file: %s", err.Error())
-	}
-
 	var (
+		line, _        = ioutil.ReadFile(os.Args[1])
 		stringSequence = strings.Split(string(line), ",")
 		sequence       = make([]int, len(stringSequence))
 	)
 
 	for i := range sequence {
-		sv := strings.TrimSpace(stringSequence[i])
-		v, _ := strconv.Atoi(sv)
-		sequence[i] = v
+		sequence[i], _ = strconv.Atoi(strings.TrimSpace(stringSequence[i]))
 	}
 
-	fmt.Println("part one")
-	run(sequence, 1)
-
-	fmt.Println("part two")
+	fmt.Println("part one:", run(sequence, 1))
+	fmt.Println("part two:", run(sequence, 2))
 	run(sequence, 2)
 }
 
-func run(sequence []int, phase int) {
-	c := computer{
-		Phase: phase,
-	}
+func run(sequence []int, input int) string {
+	c := computer{Input: input}
 
-	if c.Sequence == nil {
-		c.Sequence = make([]int, len(sequence))
-		copy(c.Sequence, sequence)
-	}
+	c.Sequence = make([]int, len(sequence))
+	copy(c.Sequence, sequence)
 
 	// Update the current computer/amplifier
 	c.process()
 
 	// This is what you get without generics.
-	fmt.Println(strings.Trim(strings.ReplaceAll(fmt.Sprint(c.Output), " ", ","), "[]"))
+	return fmt.Sprint(strings.Trim(strings.ReplaceAll(fmt.Sprint(c.Output), " ", ","), "[]"))
 }
 
 func (c *computer) process() {
-	opCode := c.Sequence[c.Pointer]
+	opCode := c.Sequence[c.Pointer] % 100
 
 	// Halt code found, stop processing.
 	if opCode == opCodeHalt {
@@ -101,149 +90,81 @@ func (c *computer) process() {
 		return
 	}
 
-	// Pointer is outside of list range.
-	if c.Pointer > len(c.Sequence) {
-		return
-	}
+	getPointer := func(argumentPosition int) int {
+		var (
+			pointer       = 0
+			modePositions = c.Sequence[c.Pointer] / 100
+			positions     = map[int]int{
+				1: modePositions % 10,
+				2: modePositions % 100 / 10,
+				3: modePositions % 1000 / 100,
+			}
+		)
 
-	expand := func(s []int, n int) []int {
-		if len(s)-1 > n {
-			return s
+		switch positions[argumentPosition] {
+		case paramModePosition:
+			pointer = c.Sequence[c.Pointer+argumentPosition]
+		case paramModeImmediate:
+			pointer = c.Pointer + argumentPosition
+		case paramModeRelative:
+			pointer = c.Sequence[c.Pointer+argumentPosition] + c.Base
 		}
 
-		var toAdd = make([]int, n-len(s)+1)
+		if pointer >= len(c.Sequence) {
+			c.Sequence = append(c.Sequence, make([]int, pointer-len(c.Sequence)+1)...)
+		}
 
-		s = append(s, toAdd...)
-
-		return s
+		return pointer
 	}
 
-	// Ensure we have at least three extra positions after the pointer to be
-	// able to assign all variables at once.
-	c.Sequence = expand(c.Sequence, c.Pointer+3)
-
-	// Get firwst two arguments, parse their input code and the actual op code
-	// for larger numbers.
-	first, second, pos := c.Sequence[c.Pointer+1], c.Sequence[c.Pointer+2], c.Sequence[c.Pointer+3]
-	firstMode, secondMode, thirdMode, opCode := parseOpCode(opCode)
-
-	c.Sequence = expand(c.Sequence, first)
-	c.Sequence = expand(c.Sequence, second)
-	c.Sequence = expand(c.Sequence, pos)
-
-	switch firstMode {
-	case paramModePosition:
-		if first >= 0 {
-			first = c.Sequence[first]
-		}
-	case paramModeImmediate:
-		break
-	case paramModeRelative:
-		// If we're storing a value, just add the destination to where.
-		if opCode == opCodeStore {
-			first = c.Base + first
-		} else {
-			first = c.Sequence[c.Base+first]
-		}
-	}
-
-	switch secondMode {
-	case paramModePosition:
-		if second >= 0 {
-			second = c.Sequence[second]
-		}
-	case paramModeImmediate:
-		break
-	case paramModeRelative:
-		second = c.Sequence[c.Base+second]
-	}
-
-	switch thirdMode {
-	case paramModePosition, paramModeImmediate:
-		break
-	case paramModeRelative:
-		pos += c.Base
+	sequenceFor := func(pos int) int {
+		return c.Sequence[getPointer(pos)]
 	}
 
 	switch opCode {
 	case opCodeAdd:
-		c.Sequence[pos] = first + second
-		c.Pointer += 4
+		c.Sequence[getPointer(3)] = sequenceFor(1) + sequenceFor(2)
 
 	case opCodeMultiply:
-		c.Sequence[pos] = first * second
-		c.Pointer += 4
+		c.Sequence[getPointer(3)] = sequenceFor(1) * sequenceFor(2)
 
 	case opCodeStore:
-		input := c.Phase
-		if c.InputCount > 0 {
-			input = c.Input
-		}
-
-		c.Sequence[first] = input
-		c.Pointer += 2
-		c.InputCount++
+		c.Sequence[getPointer(1)] = c.Input
 
 	case opCodeOutput:
-		c.Output = append(c.Output, first)
-		c.Pointer += 2
-
-		// Puase process if opcode > 4
-		if c.Phase > 4 {
-			return
-		}
+		c.Output = append(c.Output, sequenceFor(1))
 
 	case opCodeJumpIfTrue:
-		if first != 0 {
-			c.Pointer = second
-		} else {
-			c.Pointer += 3
+		if c.Sequence[getPointer(1)] != 0 {
+			c.Pointer = sequenceFor(2) - jumpMap[opCodeJumpIfTrue]
 		}
 
 	case opCodeJumpIfFalse:
-		if first == 0 {
-			c.Pointer = second
-		} else {
-			c.Pointer += 3
+		if c.Sequence[getPointer(1)] == 0 {
+			c.Pointer = sequenceFor(2) - jumpMap[opCodeJumpIfFalse]
 		}
 
 	case opCodeLessThan:
-		if first < second {
-			c.Sequence[pos] = 1
+		if c.Sequence[getPointer(1)] < c.Sequence[getPointer(2)] {
+			c.Sequence[getPointer(3)] = 1
 		} else {
-			c.Sequence[pos] = 0
+			c.Sequence[getPointer(3)] = 0
 		}
-
-		c.Pointer += 4
 
 	case opCodeEquals:
-		if first == second {
-			c.Sequence[pos] = 1
+		if c.Sequence[getPointer(1)] == c.Sequence[getPointer(2)] {
+			c.Sequence[getPointer(3)] = 1
 		} else {
-			c.Sequence[pos] = 0
+			c.Sequence[getPointer(3)] = 0
 		}
 
-		c.Pointer += 4
-
 	case opCodeAdjustBase:
-		c.Base += first
-		c.Pointer += 2
+		c.Base += sequenceFor(1)
 
 	default:
 		panic(fmt.Sprintf("unknown instruction at position %d, opCode: %d", c.Pointer, opCode))
 	}
 
+	c.Pointer += jumpMap[opCode]
 	c.process()
-}
-
-func parseOpCode(opCode int) (paramMode, paramMode, paramMode, int) {
-	var (
-		code          = opCode % 100
-		modePositions = opCode / 100
-		firstMode     = modePositions % 10
-		secondMode    = modePositions % 100 / 10
-		thirdMode     = modePositions % 1000 / 100
-	)
-
-	return paramMode(firstMode), paramMode(secondMode), paramMode(thirdMode), code
 }
