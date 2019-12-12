99b2638cc9748acf19faa6d4c94d33d9e2a03cc1
Simon Sawert <simon@sawert.se>
2019-12-12 23:26:29 +0100

    Solution for part 2 day 12

--- 12/python/main.py
+++ 12/python/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from pprint import pprint
+import math
 
 
 def main():
@@ -21,57 +21,113 @@ def main():
                 }
             )
 
-    part_one(positions)
+    step(positions, 1000)
+    step(positions)
 
 
-def part_one(positions, times=1000, debug=False):
+def step(positions, times=None):
     """
     Apply the velocity to each moon the number of times given
     """
-    velocity = [{"x": 0, "y": 0, "z": 0} for _ in range(len(positions))]
 
-    for _ in range(times):
-        for i, _ in enumerate(positions):
-            for comparison, _ in enumerate(positions):
-                if i == comparison:
-                    continue
+    # r is the cu*r*rrent state of the positions after possible gravity applied.
+    r = {"x": [], "y": [], "z": []}
 
-                for axis in ["x", "y", "z"]:
-                    if positions[i][axis] < positions[comparison][axis]:
-                        velocity[i][axis] += 1
-                    elif positions[i][axis] > positions[comparison][axis]:
-                        velocity[i][axis] -= 1
+    # To zero is the number of steps it took for the velocity to reach zero
+    # again.
+    to_zero = {"x": 0, "y": 0, "z": 0}
 
-        if debug:
-            print("velocity after cycle")
-            pprint(velocity)
+    # Populate the initial state of the positions given. This will be set as the
+    # first index. The second index is used as velocity and starts at 0.
+    for pos in positions:
+        r["x"].append([pos["x"], 0])
+        r["y"].append([pos["y"], 0])
+        r["z"].append([pos["z"], 0])
 
-        for i, _ in enumerate(velocity):
+    # The number of times the iteration has been made.
+    for c in ["x", "y", "z"]:
+        # Reset step count for each coordiante.
+        steps = 0
+
+        while True:
+            steps += 1
+
+            # Use each position as base.
+            for i, _ in enumerate(r[c]):
+                # Compare to all other positions.
+                for j, _ in enumerate(r[c]):
+                    # Exclude ourself when we compare.
+                    if i == j:
+                        continue
+
+                    # If the moon has lower position than the other moon (j),
+                    # increase velocity.
+                    if r[c][i][0] < r[c][j][0]:
+                        r[c][i][1] += 1
+
+                    # If the moon has higher position than the other moon (j),
+                    # decrease velocity.
+                    if r[c][i][0] > r[c][j][0]:
+                        r[c][i][1] -= 1
+
+            # Apply the velocity for each moon and coordinate.
+            all_zero = True
+            for i, _ in enumerate(r[c]):
+                r[c][i][0] += r[c][i][1]
+
+                # If any of the n moons didn't reach zero we're not back at the
+                # initial state.
+                if r[c][i][1] != 0:
+                    all_zero = False
+
+            # If all are zero we're back at the initial state. This only means
+            # that the velocity is back to zero. This means that half the
+            # process is done. We can use this value to calculate the iterations
+            # by multiplying the least common multiple of the coordinates with
+            # two.
+            if all_zero:
+                to_zero[c] = steps
+
+                # Stop iteration if the number of times to iterage wasn't set.
+                if times is None:
+                    break
+
+            # Stop iteration if we've iterated as many times as passed.
+            if times is not None:
+                if steps == times:
+                    break
+
+    # Part one
+    if times is not None:
+        total = 0
+
+        # Assume we have four moons to sum.
+        for i in range(4):
+            pot = 0
+            kin = 0
+
+            # Sum each axis for each moon
             for axis in ["x", "y", "z"]:
-                positions[i][axis] += velocity[i][axis]
+                pot += abs(r[axis][i][0])
+                kin += abs(r[axis][i][1])
 
-        if debug:
-            print("position after velocity")
-            show(positions)
+            # Multiply the moons potential and kinetic energy.
+            total += pot * kin
 
-    total_sum = 0
-    for i, _ in enumerate(positions):
-        pot = 0
-        kin = 0
-        moon_sum = 0
+        print("The sum of the total energy is {}".format(total))
 
-        for axis in ["x", "y", "z"]:
-            pot += abs(positions[i][axis])
-            kin += abs(velocity[i][axis])
-            moon_sum = pot * kin
+        return
 
-            positions[i]["energy"] = moon_sum
+    # Part two
+    # https://stackoverflow.com/a/42517664/2274551
+    x2, y2, z2 = to_zero["x"], to_zero["y"], to_zero["z"]
 
-        total_sum += positions[i]["energy"]
+    gcd = math.gcd(x2, y2)
+    lcm2 = y2 * z2 // gcd
+    lcm3 = x2 * lcm2 // math.gcd(x2, lcm2)
 
-    show(positions, velocity)
-
-    print(total_sum)
+    # Multiply by two to get the final number of iterations needed.
+    print("Number of iterations needed: {}".format(lcm3 * 2))
 
 
 def show(positions, velocity=None):
